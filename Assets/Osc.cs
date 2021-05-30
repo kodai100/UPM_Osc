a6b2192e6b0b7bbe2b0074e7d85a2fbe67f993aa
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-05-30 23:22:23 +0900

    rename namespace

--- Assets/Osc.cs
+++ Assets/Osc.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Text;
 
-namespace ProjectBlue.FacialCapture.Core {
+namespace Osc {
 
     using MessageQueue = System.Collections.Generic.Queue<Message>;
     
--- Assets/OscMessageEncoder.cs
+++ Assets/OscMessageEncoder.cs
@@ -4,7 +4,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace ProjectBlue.FacialCapture.Core
+namespace Osc
 {
 	public class OscMessageEncoder {
 		private string _address;
